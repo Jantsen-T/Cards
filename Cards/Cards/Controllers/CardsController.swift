eacda1aa1bd4fb857c927ac9707c4db0920ad2b1
tanmanroberts <tanmanroberts@gmail.com>
2021-06-14 13:21:07 -0600

    added the View Controllers and shaped up the storyboard a little bit. Lastly added some fake data for Gavin.

--- Cards/Cards/Controllers/CardsController.swift
+++ Cards/Cards/Controllers/CardsController.swift
@@ -15,9 +15,9 @@ class CardController {
         
         let jantsen = Card(title: "Jantsen", profilePic: <#T##UIImage#>, bio: "", codingPoints: "100/500")
         
-        let tanner = [Card(title: "Tanner", profilePic: UIImage(named: "tannerSurf")!, bio: "", codingPoints: "10 billion") ]
+        let tanner = [Card(title: "Tanner", profilePic: UIImage(named: "tannerSurf")!, bio: "This is Tanner's bio, he is really cool and great. -his mom, probably.", codingPoints: "10 billion, thousand, million, zillion") ]
         
-        let gavin = Card(title: <#T##String#>, profilePic: <#T##UIImage?#>, bio: <#T##String#>, codingPoints: <#T##String#>)
+        let gavin = Card(title: "Florida Man", profilePic: <#T##UIImage?#>, bio: "a dumpster truck wrecked my house", codingPoints: "10000")
         
         return [jantsen, tanner, gavin]
     }()
